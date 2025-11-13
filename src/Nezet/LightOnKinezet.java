b6e57ff44c16c20731b78bde74502067ee459702
S4mur419020 <muraitomi04@gmail.com>
2025-11-13 14:30:02 +0100

    mukodik a betoltes es kozepen van a jframe is

--- src/Nezet/LightOnKinezet.java
+++ src/Nezet/LightOnKinezet.java
@@ -350,6 +350,7 @@ public class LightOnKinezet extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void FelhasznalonevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_FelhasznalonevActionPerformed
